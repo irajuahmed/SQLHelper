4a6a6bbc065e606d642663f54b304b2dda539156
Raju Ahmed <rajuahmed.329@gmail.com>
2022-03-30 20:45:29 +0600

    With Transaction also
    With Transaction also Added By Raju Ahmed.

--- DBAToolKit/InsertBulkDataIntoATableUsingLoop.sql
+++ DBAToolKit/InsertBulkDataIntoATableUsingLoop.sql
@@ -1,12 +1,16 @@
 BEGIN TRAN
---SELECT * FROM StrFilings WHERE Id>10011
-
+--select * from StrFilings where Id>10011
+--SELECT COUNT(1) FROM StrTransactions--394
 DECLARE @incrementalValue BIGINT=1
+DECLARE @lastStrId BIGINT
 DECLARE @maxAlertSTR BIGINT
 SET @maxAlertSTR =(SELECT ISNULL( MAX (CasT(SUBSTRING(AlertId,4,LEN(AlertId)-2) as bigint)),0) FROM StrFilings WHERE IsCTR = 1)
 
 IF OBJECT_ID('tempdb..#TempResults') IS NOT NULL 
 DROP TABLE #TempResults;
+
+IF OBJECT_ID('tempdb..#TempTransaction') IS NOT NULL 
+DROP TABLE #TempTransaction;
 
 
 SELECT  TOP 1  
@@ -37,6 +41,37 @@ SELECT  TOP 1
 INTO #TempResults
 FROM StrFilings 
 
+
+
+SELECT  TOP 1  
+			T.[TransactionId]
+           ,T.[StrId]
+           ,T.[TransactionNo]
+           ,T.[TransactionType]
+           ,T.[AccountId]
+           ,T.[AccountOrReferenceNo]
+           ,T.[CategoryId]
+           ,T.[CategoryName]
+           ,T.[Amount]
+           ,T.[Currency]
+           ,T.[DepositorName]
+           ,T.[BranchId]
+           ,T.[Beneficiary]
+           ,T.[BenificiaryAccNo]
+           ,T.[BenificiaryBankName]
+           ,T.[BenificiaryBranchName]
+           ,T.[TransactionDate]
+           ,T.[TransactionTime]
+           ,T.[TellerId]
+           ,T.[GeoLocation]
+           ,T.[ViolatedRule]
+           ,T.[Description]
+INTO #TempTransaction
+FROM [dbo].[StrTransactions] T
+JOIN #TempResults R ON T.AccountId=R.AccountId
+
+SELECT * FROM #TempResults
+SELECT * FROM #TempTransaction
 
 WHILE(@incrementalValue<=5500)
 BEGIN
@@ -93,9 +128,59 @@ INSERT INTO [dbo].[StrFilings]
            ,[Period]
 FROM #TempResults
 
+SET @lastStrId=SCOPE_IDENTITY()
+
+INSERT INTO [dbo].[StrTransactions]
+           ([TransactionId]
+           ,[StrId]
+           ,[TransactionNo]
+           ,[TransactionType]
+           ,[AccountId]
+           ,[AccountOrReferenceNo]
+           ,[CategoryId]
+           ,[CategoryName]
+           ,[Amount]
+           ,[Currency]
+           ,[DepositorName]
+           ,[BranchId]
+           ,[Beneficiary]
+           ,[BenificiaryAccNo]
+           ,[BenificiaryBankName]
+           ,[BenificiaryBranchName]
+           ,[TransactionDate]
+           ,[TransactionTime]
+           ,[TellerId]
+           ,[GeoLocation]
+           ,[ViolatedRule]
+           ,[Description])
+
+		SELECT   [TransactionId]
+				,@lastStrId [StrId]
+				,[TransactionNo]
+				,[TransactionType]
+				,[AccountId]
+				,[AccountOrReferenceNo]
+				,[CategoryId]
+				,[CategoryName]
+				,[Amount]
+				,[Currency]
+				,[DepositorName]
+				,[BranchId]
+				,[Beneficiary]
+				,[BenificiaryAccNo]
+				,[BenificiaryBankName]
+				,[BenificiaryBranchName]
+				,[TransactionDate]
+				,[TransactionTime]
+				,[TellerId]
+				,[GeoLocation]
+				,[ViolatedRule]
+				,[Description]
+		FROM #TempTransaction
+ 
  SET @incrementalValue += 1
 
 END
---SELECT * FROM StrFilings WHERE Id>10011
+--select * from StrFilings where Id>10011
 ROLLBACK
 
